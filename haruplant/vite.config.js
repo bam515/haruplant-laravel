a23e8e3a48886613bceedfdeab6fb96a705eaf87
bam <devbam515@gmail.com>
2025-05-14 13:45:29 +0900

    chore: rename project folder from app to haruplant

--- haruplant/vite.config.js
+++ haruplant/vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/haruplant.css', 'resources/js/haruplant.js'],
             refresh: true,
         }),
     ],
