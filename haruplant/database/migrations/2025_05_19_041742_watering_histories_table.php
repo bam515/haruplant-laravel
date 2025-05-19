86b0cbdfde90a87e7a84b3f06cb6405a867a36f1
bam <devbam515@gmail.com>
2025-05-19 13:28:30 +0900

    feat: define initial DB schema for HaruPlant MVP (MVP를 위한 초기 스키마 마이그레이션 파일 정의)

--- haruplant/database/migrations/2025_05_19_041742_watering_histories_table.php
+++ haruplant/database/migrations/2025_05_19_041742_watering_histories_table.php
@@ -11,11 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('watering_histories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->foreignId('plant_id')->constrained()->onDelete('cascade');
+            $table->dateTime('watered_at');
+            $table->text('memo')->nullable();
             $table->timestamps();
         });
     }
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('watering_histories');
     }
 };
